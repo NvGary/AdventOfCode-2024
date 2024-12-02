f85fc86a6ef836912262e5567c3e9511b78d5197
Gary Nicholson <gary@agilenv.com>
2024-12-02 12:08:16 +0000

    day 2 solution

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { calcDifference, calcSimilarity, loadFromFile } from './utils/locationId';
+import { calcDifference, calcSimilarity, loadFromFile as loadLocationIdsFromFile } from './utils/locationId';
+import { isSafe, isSafeWithDampener, loadFromFile as loadReportsFromFile } from './utils/report';
 
 const day1 = () => {
   console.log('-- DAY 1 --');
   
-  const [left, right] = loadFromFile('./lib/location-ids.txt');
+  const [left, right] = loadLocationIdsFromFile('./lib/location-ids.txt');
 
   console.log(`Location Id difference is: ${calcDifference(left.sort(), right.sort())}`);
   console.log(`Location Id similarity is: ${calcSimilarity(left, right)}`);
@@ -12,6 +13,10 @@ const day1 = () => {
 const day2 = () => {
   console.log('-- DAY 2 --');
 
+  const reports = loadReportsFromFile('./lib/reports.txt');
+
+  console.log(`Safe report count: ${reports.map(isSafe).filter(res => res === true).length}`);
+  console.log(`Safe (with dampener) report count: ${reports.map(isSafeWithDampener).filter(res => res === true).length}`);
 }
 
 (() => {
