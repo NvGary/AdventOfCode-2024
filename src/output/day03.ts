0a1fff7a179ce4ce82fb52b4e12c38eee593c81b
Gary Nicholson <gary@agilenv.com>
2024-12-05 22:50:04 +0000

    refactor daily output

---
+++ src/output/day03.ts
@@ -0,0 +1,10 @@
+import { calculateMuls, loadFromFile } from '../utils/mul';
+
+export const day03 = () => {
+    console.log('--- Day 3: Mull It Over ---');
+  
+    // 191183308
+    console.log(`Multiplication results (commands disabled): ${calculateMuls(loadFromFile('./lib/muls.txt', false))}`);
+    // 92082041
+    console.log(`Multiplication results (commands enabled): ${calculateMuls(loadFromFile('./lib/muls.txt'))}`);
+}
