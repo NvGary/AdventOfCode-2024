1fca4197adfc136a4d74585b2380a8d7a3170c7d
Gary Nicholson <NvGary@users.noreply.github.com>
2024-12-06 17:24:22 +0000

    day 6 solution (#8)

---
+++ src/utils/map.test.ts
@@ -0,0 +1,43 @@
+import { advanceTime, countVisited, createDejavu, loadFromFile } from './map';
+
+const grid = [
+    ['.','.','.','.','#','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.','.','#'],
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','.','#','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','#','.','.'],
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','#','.','.','^','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.','#','.'],
+    ['#','.','.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','#','.','.','.'],
+];
+
+describe('report utils', () => {
+    describe('function loadFromFile', () => {
+        it('loads data correctly', () => {
+            const data = loadFromFile('./lib/test/map.txt');
+            expect(data).toHaveLength(10);
+            data.forEach(row => {
+                expect(row).toHaveLength(10);
+            });
+            expect(data).toStrictEqual(grid);
+        });
+    });
+
+    describe('function advanceTime', () => {
+        it('moves the guard predictably', () => {
+            const data = loadFromFile('./lib/test/map.txt');
+            const { map: res } = advanceTime(data);
+            expect(countVisited(res)).toBe(41);
+        });
+    });
+
+    describe('function createDejavu', () => {
+        it('correctly predicts dejavu', () => {
+            const data = loadFromFile('./lib/test/map.txt');
+            const { map: res } = advanceTime(data);
+            expect(createDejavu(res)).toBe(6);
+        });
+    });
+});
