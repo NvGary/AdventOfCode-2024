7f533825f6ac8eded11965a0fef71261ba9b6a7e
Gary Nicholson <NvGary@users.noreply.github.com>
2024-12-09 10:11:35 +0000

    lint improvements (#14)

--- src/utils/print-queue.ts
+++ src/utils/print-queue.ts
@@ -23,24 +23,27 @@ export const loadFromFile = (filename: string): PrintQueue => {
     return { ordering, production };
 };
 
-const _hasCorrectOrdering = (production: PrintQueue['production'][number], ordering: PrintQueue['ordering']): boolean => production.reduce<boolean>((acc, cur, idx) =>
-// console.log({ acc, cur, idx });
-    acc && production.slice(idx + 1).reduce<boolean>((acc2, cur2) =>
-    // console.log({ acc2, cur2, 'ordering includes': [cur, cur2], res: ordering.some(([l,r]) => cur === l && cur2 === r) });
-        acc2 && ordering.some(([l, r]) => cur === l && cur2 === r)
-    , true)
-, true);
+const hasCorrectOrdering_impl = (production: PrintQueue['production'][number], ordering: PrintQueue['ordering']): boolean =>
+    production.reduce<boolean>((acc, cur, idx) =>
+    // console.log({ acc, cur, idx });
+        acc && production.slice(idx + 1).reduce<boolean>((acc2, cur2) =>
+        // console.log({ acc2, cur2, 'ordering includes': [cur, cur2], res: ordering.some(([l,r]) => cur === l && cur2 === r) });
+            acc2 && ordering.some(([l, r]) => cur === l && cur2 === r)
+        , true)
+    , true);
 
 export const hasCorrectOrdering = ({ ordering, production }: PrintQueue): PrintQueue['production'] =>
     // console.log({ production, ordering });
-    production.filter(entry => _hasCorrectOrdering(entry, ordering));
+    production.filter(entry => hasCorrectOrdering_impl(entry, ordering));
 
 export const hasIncorrectOrdering = ({ ordering, production }: PrintQueue): PrintQueue['production'] =>
     // console.log({ production, ordering });
-    production.filter(entry => !_hasCorrectOrdering(entry, ordering));
+    production.filter(entry => !hasCorrectOrdering_impl(entry, ordering));
 
 export const calculateProductionTotals = (production: PrintQueue['production']): number => production.reduce<number>((acc, cur) => acc + cur[(cur.length - 1) / 2], 0);
 
-const _correctOrdering = (production: PrintQueue['production'][number], ordering: PrintQueue['ordering']): PrintQueue['production'][number] => production.sort((a, b) => ordering.some(([l, r]) => a === l && b === r) ? -1 : ordering.some(([r, l]) => a === l && b === r) ? 1 : 0);
-
-export const correctOrdering = ({ ordering, production }: PrintQueue): PrintQueue['production'] => production.filter(entry => _hasCorrectOrdering(_correctOrdering(entry, ordering), ordering));
+export const correctOrdering = ({ ordering, production }: PrintQueue): PrintQueue['production'] =>
+    production.filter(entry => hasCorrectOrdering_impl(
+        entry.sort((a, b) => ordering.some(([l, r]) => a === l && b === r) ? -1 : ordering.some(([r, l]) => a === l && b === r) ? 1 : 0),
+        ordering
+    ));
