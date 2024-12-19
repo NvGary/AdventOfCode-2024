5a05f5d5cbd0ff750edaa8ec73df4231c37f45c5
Gary Nicholson <gary@agilenv.com>
2024-12-19 16:59:37 +0000

    reusable memoise

--- src/utils/16-20/linen.ts
+++ src/utils/16-20/linen.ts
@@ -1,4 +1,5 @@
 import { readFileByLine } from '../fs';
+import { memoise } from '../memoise';
 
 type Towels = {
     patterns: string[];
@@ -21,34 +22,6 @@ export const loadFromFile = (filename: string): Towels => {
     return { patterns, designs };
 };
 
-/**
- * Memoizes the input of the function and caches the results in a hash map.
- * Must be written like this to make it's scope global to the module
- * @param {*} func The function to cache the result of
- * @returns The result of the inputs
- */
-const memoise = (func: Function): typeof func => {
-    // Create a cache. Using a hash map is exponentially faster than a plain object
-    const cache = new Map();
-
-    return function r(this: unknown, ...args: never[]) {
-        // Get the JSON string of the args
-        const [key] = args;
-
-        // Check if this value has been cached and return it if found
-        if (cache.has(key)) {
-            return cache.get(key);
-        }
-
-        // Otherwise run function and get the result to cache
-        // eslint-disable-next-line no-invalid-this
-        const result = func.apply(this, args);
-        cache.set(key, result);
-
-        return result;
-    };
-};
-
 const solve = memoise((design: Towels['designs'][number], patterns: Towels['patterns']): number => {
     let solutionCount = 0;
 
@@ -60,7 +33,7 @@ const solve = memoise((design: Towels['designs'][number], patterns: Towels['patt
         }
         else if (design.startsWith(pattern)) {
             const remainingDesign = design.slice(pattern.length);
-            solutionCount += solve(remainingDesign, patterns) as number;
+            solutionCount += solve(remainingDesign, patterns);
         }
     }
 
